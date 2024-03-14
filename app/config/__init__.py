ffe618d7ff062eaaf29646da09377c2dc9bdbfe0
Nickitarius <pilnov.nickita@yandex.ru>
2024-03-14 18:31:35 +0800

    testing many to many

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,4 +1,4 @@
-from model.weather_events_meteo_records import WeatherEventsMeteoRecords
+# from model.weather_events_meteo_records import WeatherEventsMeteoRecords
 from model.weather_event import WeatherEvent
 from model.meteo_record import MeteoRecord
 
--- app/model/weather_events_meteo_records.py
+++ app/model/weather_events_meteo_records.py
@@ -1,19 +1,19 @@
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from config.db import DB
+# from sqlalchemy import ForeignKey
+# from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from config.db import DB
 
 
-class WeatherEventsMeteoRecords(DB):
-    __tablename__ = "weather_events_meteo_records"
+# class WeatherEventsMeteoRecords(DB):
+#     __tablename__ = "weather_events_meteo_records"
 
-    weather_event_id: Mapped[int] = mapped_column(
-        ForeignKey("weather_events.id"), primary_key=True)
-    meteo_record_id: Mapped[int] = mapped_column(
-        ForeignKey("meteo_records.id"), primary_key=True)
+#     weather_event_id: Mapped[int] = mapped_column(
+#         ForeignKey("weather_events.id"), primary_key=True)
+#     meteo_record_id: Mapped[int] = mapped_column(
+#         ForeignKey("meteo_records.id"), primary_key=True)
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+#     id: Mapped[int] = mapped_column(primary_key=True)
 
-    meteo_record: Mapped["MeteoRecord"] = relationship(
-        back_populates="weather_events")
-    weather_event: Mapped["WeatherEvent"] = relationship(
-        back_populates="meteo_records")
+#     meteo_record: Mapped["MeteoRecord"] = relationship(
+#         back_populates="weather_events")
+#     weather_event: Mapped["WeatherEvent"] = relationship(
+#         back_populates="meteo_records")
